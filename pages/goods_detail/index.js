5b622ffc7d62374bea6c0f0a80906bf39d9cce38
13751139402 <48110405+13751139402@users.noreply.github.com>
2020-03-21 18:18:20 +0800

    2020/3/21

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -12,8 +12,17 @@
     2 点击轮播图 预览大图
       1 给轮播图绑定点击事件
       2 调用小程序的api previewImage
+    3 点击 加入购物车
+      1 先绑定点击事件
+      2 获取缓存中的购物车数据 数组格式
+      3 先判断 当前的商品是否存在于 购物车
+      4 已经存在 修改商品数据  执行购物车数量++ 重新把购物车数组 填充回缓存中
+      5 不存在于购物车的数组中 直接给购物车数组添加一个新元素 新元素 带上 购买数量属性 num  重新把购物车数组 填充回缓存中
+      6 弹出提示
  */
-import { request } from '../../request/index.js';
+import {
+  request
+} from '../../request/index.js';
 import regeneratorRuntime from '../../lib/runtime/runtime.js';
 
 Page({
@@ -22,7 +31,9 @@ Page({
   },
   // 商品对象
   GoodsInfo: {},
-  onLoad: function({ goods_id }) {
+  onLoad: function({
+    goods_id
+  }) {
     this.getGoodsDetail(goods_id);
   },
   // 获取商品详情数据
@@ -33,7 +44,12 @@ Page({
         goods_id
       }
     });
-    const { goods_name, goods_price, goods_introduce, pics } = goodsObj;
+    const {
+      goods_name,
+      goods_price,
+      goods_introduce,
+      pics
+    } = goodsObj;
     this.GoodsInfo = goodsObj;
     this.setData({
       goodsObj: {
@@ -48,12 +64,43 @@ Page({
     });
   },
   // 点击轮播图 放大预览
-  handlePrevewImage({ currentTarget:{dataset:{url:current}}}) {
+  handlePrevewImage({
+    currentTarget: {
+      dataset: {
+        url: current
+      }
+    }
+  }) {
     // 1 先构造要预览的图片数组
     const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
     wx.previewImage({
       current,
       urls
     });
+  },
+  // 点击 加入购物车
+  handleCartAdd() {
+    console.log("gouwuche")
+    // 1 获取缓存中的购物车 数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 2 判断 商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      // 3.不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo);
+    } else {
+      // 4.已经存在购物车数据 执行 num++
+      cart[index].num++;
+    }
+    // 5 把购物车重新添加回缓存中
+    wx.setStorageSync("cart", cart)
+    // 6 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon:"success",
+      // true 防止用户 手抖 疯狂点击按钮
+      mask:false
+    })
   }
 });
