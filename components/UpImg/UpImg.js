bdc4bff63403c21e0e2f06ac67e6c8c48e7bf10d
嘉爷 <48110405+13751139402@users.noreply.github.com>
2020-03-27 23:30:46 +0800

    2020/3/27

---
+++ components/UpImg/UpImg.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-27 22:17:35
+ * @LastEditTime: 2020-03-27 22:57:01
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \heima-mall\components\UpImg\UpImg.js
+ */
+// components/UpImg/UpImg.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    src: {
+      type: String,
+      define: ""
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,35 @@
 /*
  * @Author: your name
- * @Date: 2020-03-16 10:56:11
- * @LastEditTime: 2020-03-27 16:58:19
+ * @Date: 2020-03-19 21:03:47
+ * @LastEditTime: 2020-03-27 23:27:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \ heima-mall\pages\feedback\index.js
+ * @FilePath: \heima-mall\pages\feedback\index.js
  */
-// pages/feedback/index.js
+/**
+ * 1 点击"+"触发tap点击事件
+ *   1 调用小程序内置的 选择图片的api
+ *   2 获取到 图片的路径 数组
+ *   3 把图片路径 存到data的变量中
+ *   4 页面就可以根据 图片数组 进行循环显示 自定义组件
+ * 2 点击 自定义图片 组件
+ *   1 获取被点击的元素的索引
+ *   2 获取data中的图片数组
+ *   3 根据索引 数组中删除对应的元素
+ *   4 把数组重新设置回data中
+ * 3 点击"提交"
+ *   1 获取文本域的内容 类似 输入框的获取
+ *     1 data中定义变量 表示 输入框内容
+ *     2 文本域 绑定 输入事件 事件触发的时候 把输入框的值 存入到变量中
+ *   2 对这些内容 合法性验证
+ *   3 验证通过 用户选择的图片 上传到专门的图片的服务器 返回图片外网的链接
+ *     1 遍历图片数组
+ *     2 挨个上传
+ *     3 自己再维护图片数组 存放 图片上传后的外网的链接
+ *   4 文本域 和 外网的图片的路径 一起提交到服务器中 前端的模拟 不会发送请求到后台...
+ *   5 清空当前页面
+ *   6 返回上一页
+ */
 Page({
 
   /**
@@ -24,6 +47,9 @@ Page({
       isActive: false
     },
     ],
+    chooseImgs: [],
+    // 文本域的内容
+    textVal: ""
   },
   // 标签点击事件，从子组件传递过来
   handleTabsItemChange(e) {
@@ -40,5 +66,75 @@ Page({
     this.setData({
       tabs
     })
+  },
+  // 点击 "+" 选择图片
+  handleChooseImg() {
+    // 2 调用小程序内置的选择图片api
+    wx.chooseImage({
+      // 同时选中的图片的数量
+      count: 9,
+      // 图片的格式 原图 压缩
+      sizeType: ['original', 'compressed'],
+      // 图片的来源 相册 照相机
+      sourceType: ['album', 'camera'],
+      success: (result) => {
+        console.log(this);
+        this.setData({
+          // 图片数组 进行拼接
+          chooseImgs: [...this.data.chooseImgs, ...result.tempFilePaths]
+        })
+      },
+      fail: () => { },
+      complete: () => { }
+    });
+  },
+  // 点击 自定义图片组件
+  handleRemoveImg(e) {
+    // 2 获取被点击的组件的索引
+    const { index } = e.currentTarget.dataset;
+    // 3 获取data中的图片数组
+    let { chooseImgs } = this.data;
+    // 4 删除元素
+    chooseImgs.splice(index, 1)
+    this.setData({
+      chooseImgs
+    })
+  },
+  // 文本域的输入事件
+  handleTextInput(e) {
+    this.setData({
+      textVal: e.detail.value
+    })
+  },
+  // 提交按钮的点击
+  handleFormSubmit() {
+    // 1 获取文本域的内容
+    const { textVal,chooseImgs } = this.data;
+    // 2 合法性的验证
+    if (!textVal.trim()) {
+      // 不合法
+      wx.showToast({
+        title: '输入不合法',
+        icon: 'none',
+        mask: true,
+      });
+    }
+    // 3 准备上传图片 到专门的图片服务器
+    // 上传文件的api 不支持 多个文件同时上传 遍历数组 挨个上传
+    wx.uploadFile({
+      // 图片要上传到哪里
+      url: '',
+      // 被上传的文件的路径
+      filePath: "",
+      // 上传的文件的名称 后台来获取文件 file
+      name: "",
+      // 顺带的文本信息
+      formData: {},
+      success: (result) => {
+
+      },
+      fail: () => { },
+      complete: () => { }
+    });
   }
 })
